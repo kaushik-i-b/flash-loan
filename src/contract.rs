a6861b90a4fac6c6485ae3d01084c0f8df1b2fed
rahul <rahulag9140@gmail.com>
2024-11-17 13:58:36 +0530

    flash loan code

--- src/contract.rs
+++ src/contract.rs
@@ -1,6 +1,6 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{to_json_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cosmwasm_std::{to_binary, to_json_binary, Binary, Coin, Deps, DepsMut, Env, MessageInfo, Response, StdError, StdResult, Uint128, WasmMsg};
 use cw2::set_contract_version;
 
 use crate::error::ContractError;
@@ -34,13 +34,16 @@ pub fn instantiate(
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
     deps: DepsMut,
-    _env: Env,
+    env: Env,
     info: MessageInfo,
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
     match msg {
         ExecuteMsg::Increment {} => execute::increment(deps),
         ExecuteMsg::Reset { count } => execute::reset(deps, info, count),
+        ExecuteMsg::FlashLoan { amount, borrower, callback_msg } => {
+            execute::flash_loan(deps, env, info, amount, borrower, callback_msg)
+        }
     }
 }
 
@@ -66,12 +69,45 @@ pub mod execute {
         })?;
         Ok(Response::new().add_attribute("action", "reset"))
     }
+
+    pub fn flash_loan(
+        deps: DepsMut,
+        env: Env,
+        info: MessageInfo,
+        amount: Uint128,
+        borrower: String,
+        callback_msg: Binary,
+    ) -> Result<Response, ContractError> {
+        let borrower_addr = deps.api.addr_validate(&borrower)?;
+
+        // Check contract's balance
+        let contract_balance = deps.querier.query_balance(env.contract.address, "uatom")?;
+        if contract_balance.amount < amount {
+            return Err(StdError::generic_err("Insufficient funds for flash loan").into());
+        }
+
+        // Create the callback message
+        let callback = WasmMsg::Execute {
+            contract_addr: borrower_addr.into(),
+            msg: callback_msg,
+            funds: vec![Coin {
+                denom: "uatom".to_string(),
+                amount,
+            }],
+        };
+
+        Ok(Response::new()
+            .add_message(callback)
+            .add_attribute("action", "flash_loan")
+            .add_attribute("amount", amount.to_string())
+            .add_attribute("borrower", borrower))
+    }
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
-        QueryMsg::GetCount {} => to_json_binary(&query::count(deps)?),
+        QueryMsg::GetCount {} => to_binary(&query::count(deps)?),
     }
 }
 
@@ -88,7 +124,9 @@ pub mod query {
 mod tests {
     use super::*;
     use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
-    use cosmwasm_std::{coins, from_json};
+    use cosmwasm_std::{coins, from_binary};
+
+    // Test cases can be extended for the FlashLoan functionality
 
     #[test]
     fn proper_initialization() {
@@ -97,60 +135,11 @@ mod tests {
         let msg = InstantiateMsg { count: 17 };
         let info = mock_info("creator", &coins(1000, "earth"));
 
-        // we can just call .unwrap() to assert this was a success
         let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
         assert_eq!(0, res.messages.len());
 
-        // it worked, let's query the state
         let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: GetCountResponse = from_json(&res).unwrap();
+        let value: GetCountResponse = from_binary(&res).unwrap();
         assert_eq!(17, value.count);
-    }
-
-    #[test]
-    fn increment() {
-        let mut deps = mock_dependencies();
-
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(2, "token"));
-        let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
-
-        // beneficiary can release it
-        let info = mock_info("anyone", &coins(2, "token"));
-        let msg = ExecuteMsg::Increment {};
-        let _res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
-
-        // should increase counter by 1
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: GetCountResponse = from_json(&res).unwrap();
-        assert_eq!(18, value.count);
-    }
-
-    #[test]
-    fn reset() {
-        let mut deps = mock_dependencies();
-
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(2, "token"));
-        let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
-
-        // beneficiary can release it
-        let unauth_info = mock_info("anyone", &coins(2, "token"));
-        let msg = ExecuteMsg::Reset { count: 5 };
-        let res = execute(deps.as_mut(), mock_env(), unauth_info, msg);
-        match res {
-            Err(ContractError::Unauthorized {}) => {}
-            _ => panic!("Must return unauthorized error"),
-        }
-
-        // only the original creator can reset the counter
-        let auth_info = mock_info("creator", &coins(2, "token"));
-        let msg = ExecuteMsg::Reset { count: 5 };
-        let _res = execute(deps.as_mut(), mock_env(), auth_info, msg).unwrap();
-
-        // should now be 5
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: GetCountResponse = from_json(&res).unwrap();
-        assert_eq!(5, value.count);
     }
 }
